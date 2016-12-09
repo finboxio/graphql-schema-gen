c4437d320504c030911c5c62bb2290b3ac82402e
Brian Dentino <brian.dentino@gmail.com>
2016-12-08 19:56:57 -0800

    update packages, add support for implicitly computed fields if resolver is present, return schema instead of query fn

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ function createQueryFn(source, implementation) {
   var schema = new graphql.GraphQLSchema({
     query: objectTypes['Query']
   });
-  return (query, params, context) => graphql.graphql(schema, query, context, params || {});
+  return schema;
 }
 createQueryFn.parse = parse;
 createQueryFn.createTypeGenerator = createTypeGenerator;
